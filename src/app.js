c52726c35aac19079f876742b1a2c35619485285
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2018-10-25 14:17:11 -0300

    Configuração de acesso
    Configuração dos headers da api, com a origem e métodos

--- src/app.js
+++ src/app.js
@@ -6,4 +6,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Configuração de acesso
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+})
+
 module.exports = app;
