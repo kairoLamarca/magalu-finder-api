ecee2f4659a3c8ee4c2c2b6020f98e6ad165c92a
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2018-10-25 14:12:25 -0300

    instalação e configuração do body parser
    configuração do body parser para aceitar json

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 module.exports = app;
